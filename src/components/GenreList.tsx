a60d32865c0d57999c09e6545dd37b1c268dacfe
Ahmed0107 <ahmed.sameh01094@gmail.com>
2024-05-25 14:35:58 +0300

    Filter Games by Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import useGenres from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import getCroppedImgUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectedGenre: (genra: Genre | null) => void
+}
+
+const GenreList = ({onSelectedGenre} : Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -20,7 +23,8 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImgUrl(g.image_background)}
             />
-            <Text fontSize="lg">{g.name}</Text>
+            <Button onClick={() => onSelectedGenre(g)
+            } fontSize="lg" variant='link'>{g.name}</Button>
           </HStack>
         </ListItem>
       ))}
--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 
 interface FetchResponse<T> {
@@ -8,7 +8,7 @@ interface FetchResponse<T> {
   results: T[];
 }
 
-function useData <T>(endpoint: string) {
+function useData <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?:any[]) {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -18,7 +18,7 @@ function useData <T>(endpoint: string) {
 
     setIsLoading(true);
     apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
       .then((response) => {
         setData(response.data.results);
         setIsLoading(false);
@@ -30,7 +30,7 @@ function useData <T>(endpoint: string) {
       });
 
     return () => controller.abort();
-  }, []);
+  }, deps ? [...deps]: []);
 
   return { data, error, isLoading };
 }
